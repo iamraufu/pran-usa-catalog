8c90b8cef84c9d7670e0e492f407fc20828d2a0f
raufu.dev <raufu.dev@gmail.com>
2026-08-03 20:50:45 -0400

    fix product grids

--- src/App.jsx
+++ src/App.jsx
@@ -189,10 +189,10 @@ bg-gray-100
 grid
 grid-cols-1
 sm:grid-cols-2
-md:grid-cols-3
-lg:grid-cols-4
-xl:grid-cols-5
-2xl:grid-cols-6
+md:grid-cols-2
+lg:grid-cols-3
+xl:grid-cols-3
+2xl:grid-cols-4
 gap-5
 "
         >
